699c823328ab66862f6ea3cddee1aa4d59352cf3
User <User@DESKTOP-327J9SQ>
2022-11-26 17:45:10 -0600

    Agregar archivos de proyecto.

---
+++ Formulatio_Actividad/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Formulatio_Actividad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.ForeColor = System.Drawing.Color.Lavender;
+            this.button2.Image = global::Formulatio_Actividad.Properties.Resources.registrotab;
+            this.button2.Location = new System.Drawing.Point(391, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 139);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "REGISTO DE \r\nPROVEEDORES";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Image = global::Formulatio_Actividad.Properties.Resources.Recurso_12;
+            this.button1.Location = new System.Drawing.Point(13, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 139);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "REGISTRO DE\r\n PRODUCTOS";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.YellowGreen;
+            this.button3.Image = global::Formulatio_Actividad.Properties.Resources.Recurso_4;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(202, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button3.Size = new System.Drawing.Size(181, 139);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "REGISTRO DE \r\nCLIENTES\r\n";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.ForeColor = System.Drawing.Color.Lavender;
+            this.button4.Image = global::Formulatio_Actividad.Properties.Resources.FOLDER;
+            this.button4.Location = new System.Drawing.Point(13, 145);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 139);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "REGISTO DE \r\nCATEGORIAS\r\n";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MediumPurple;
+            this.button5.ForeColor = System.Drawing.Color.Lavender;
+            this.button5.Image = global::Formulatio_Actividad.Properties.Resources._111111;
+            this.button5.Location = new System.Drawing.Point(202, 145);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 139);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "REGISTRO DE\r\nEGRESOS\r\n";
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button6.ForeColor = System.Drawing.Color.Lavender;
+            this.button6.Image = global::Formulatio_Actividad.Properties.Resources.MONEY;
+            this.button6.Location = new System.Drawing.Point(394, 145);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(184, 139);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "REGISTO DE \r\nCAJA\r\n";
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button7.ForeColor = System.Drawing.Color.Lavender;
+            this.button7.Image = global::Formulatio_Actividad.Properties.Resources.GRAFICA;
+            this.button7.Location = new System.Drawing.Point(13, 290);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(184, 139);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "REGISTRO DE VENTAS\r\n";
+            this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Maroon;
+            this.button8.ForeColor = System.Drawing.Color.Lavender;
+            this.button8.Image = global::Formulatio_Actividad.Properties.Resources.Carrito;
+            this.button8.Location = new System.Drawing.Point(202, 290);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(184, 139);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "REGISTRAR\r\nCOMPRAS\r\n";
+            this.button8.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button9.ForeColor = System.Drawing.Color.Lavender;
+            this.button9.Image = global::Formulatio_Actividad.Properties.Resources.HOMBRECITO1;
+            this.button9.Location = new System.Drawing.Point(394, 290);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(184, 139);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "INFORME DE\r\nINGRESO DE CAJA\r\n";
+            this.button9.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Tomato;
+            this.button10.ForeColor = System.Drawing.Color.Lavender;
+            this.button10.Image = global::Formulatio_Actividad.Properties.Resources.TABLITA;
+            this.button10.Location = new System.Drawing.Point(13, 435);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(184, 139);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "VENTAS\r\nREALIZADAS\r\n\r\n";
+            this.button10.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Olive;
+            this.button11.ForeColor = System.Drawing.Color.Lavender;
+            this.button11.Image = global::Formulatio_Actividad.Properties.Resources.COMPRAS;
+            this.button11.Location = new System.Drawing.Point(202, 435);
+            this.button11.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(184, 139);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "COMPRAS \r\nREALIZADAS\r\n";
+            this.button11.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button12.ForeColor = System.Drawing.Color.Lavender;
+            this.button12.Image = global::Formulatio_Actividad.Properties.Resources.GRAPH;
+            this.button12.Location = new System.Drawing.Point(394, 435);
+            this.button12.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(184, 139);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "VENTAS MENSUALES";
+            this.button12.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Formulatio_Actividad.Properties.Resources.GRAFICA;
+            this.ClientSize = new System.Drawing.Size(933, 627);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
